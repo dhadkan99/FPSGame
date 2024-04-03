4521f99ddee9ebb036d9c506f4bddd70500805b3
Dhadkan <dhadkankc88@gmail.com>
2024-04-03 14:43:55 +0545

    added game project

---
+++ fps game/Assets/InputSystem/Gun.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+
+
+public class Gun : MonoBehaviour
+{
+    private StarterAssetsInputs _input;
+    [SerializeField]
+    private GameObject bulletPrefabs;
+    [SerializeField]
+    private GameObject bulletpoint;
+    [SerializeField]
+    private float bulletSpeed = 600;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _input = transform.root.GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_input.shoot)
+        {
+            Shoot();
+            _input.shoot = false;
+        }
+    }
+    void Shoot()
+    {
+        Debug.Log("shoot!");
+        GameObject bullet = Instantiate(bulletPrefabs, bulletpoint.transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+        Destroy(bullet, 1);
+       
+    }
+
+}
